38aae811ee8c71288a391892b68af31b7c4fe69b
Levi Starrett <levi@roxsoftware.com>
2014-07-08 11:36:08 -0700

    commented out database initialization for the sake of the demo

--- application/app.js
+++ application/app.js
@@ -43,12 +43,14 @@ fs.readFile("config.json", 'utf8', function(err, data) {
     console.log(configName);
     console.log("Server port: " + defaultPort);
     
+    /*
     db.sequelize(config[configName], function(err) {
         if (err) {
             console.log("Sequelize err: " + err[0]);
             process.exit(-1);
         }
         else {
+    */
             // all environments
             process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
             
@@ -89,6 +91,8 @@ fs.readFile("config.json", 'utf8', function(err, data) {
             http.createServer(app).listen(app.get('port'), function(){
                 console.log('Express server listening on port ' + app.get('port'))
             });
+    /*
         }
     });
+    */
 });
