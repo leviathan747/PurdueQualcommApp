4a4e2cb63cfc57ce79cf1b80c2fa863233dd41f4
Kirby Kohlmorgen <kirby@purdue.edu>
2014-08-19 23:30:44 -0400

    Add basic leaderboard

--- application/app.js
+++ application/app.js
@@ -94,6 +94,7 @@ fs.readFile("config.json", 'utf8', function(err, data) {
             app.get('/events', pages.events);
             app.get('/tech', pages.tech);
             app.get('/trivia', isLoggedIn, pages.trivia);
+            app.get('/trivia/leaderboard', isLoggedIn, pages.leaderboard);
             app.get('/trivia/:id', isLoggedIn, pages.triviaQuestion);
             app.get('/connect', isLoggedIn, pages.connect);
             app.get('/profile', isLoggedIn, pages.profile);
